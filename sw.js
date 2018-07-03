4c360f593bb3d84100272bdb632dda98fd90babe
Sebastian Klingler <sklingler93@gmail.com>
2018-07-02 20:03:22 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1ce8d6b155ddf7772377.js","5a569aa8e5ecbce082e9a73c0006d70d"],["/component---node-modules-gatsby-plugin-offline-app-shell-js-9627945674b321be685e.js","6334f0ea6e36556ec71fbc6f1308d3de"],["/index.html","ae6e0064a9e69299471f415a07c7ee9e"],["/offline-plugin-app-shell-fallback/index.html","eb9ed5ff036637c0c3a5b218b4f1f53e"],["/webpack-runtime-63edc8e495408e9ebfb9.js","590f88b491b8a5ab567842cec4fe71de"]];
+var precacheConfig = [["/app-1ce8d6b155ddf7772377.js","5a569aa8e5ecbce082e9a73c0006d70d"],["/component---node-modules-gatsby-plugin-offline-app-shell-js-9627945674b321be685e.js","6334f0ea6e36556ec71fbc6f1308d3de"],["/index.html","204fa69092c6c0db9e5d0ec121599065"],["/offline-plugin-app-shell-fallback/index.html","5fea65977699d1df9162b80a6e7b3e8a"],["/webpack-runtime-612a6fce23463c5caf88.js","a145bc416613a1639bd173d34875f538"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
